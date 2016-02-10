b7e88dcf8ec56a5a6842263788d2cc62f832d9fa
illukonin <ilya.lukonin@Gmail.com>
2016-02-10 18:05:49 +0100

    Update plotnuclei3D_ellipse.m
    added color coding for the z axis

--- plotnuclei3D_ellipse.m
+++ plotnuclei3D_ellipse.m
@@ -10,11 +10,11 @@ colours=[];
 orientations=[];
 
 for i=1:length(unique(kIn.Metadata_Field))
-
+    
     j= uniqueFields(i);
     subarray=zeros(length(find(kIn.Metadata_Field==j)),4);
     subarray(:,1)=find(kIn.Metadata_Field==j);
-        
+    
     centersXtemp =kIn.Location_Center_X(kIn.Metadata_Field==j)+kIn.WellCoordX(kIn.Metadata_Field==j);
     centersYtemp =kIn.Location_Center_Y(kIn.Metadata_Field==j)+kIn.WellCoordY(kIn.Metadata_Field==j);
     centersZtemp =kIn.Metadata_Plane(kIn.Metadata_Field==j);
@@ -23,11 +23,11 @@ for i=1:length(unique(kIn.Metadata_Field))
     majorstemp = kIn.AreaShape_MajorAxisLength(kIn.Metadata_Field==j)/2;
     minorstemp= kIn.AreaShape_MinorAxisLength(kIn.Metadata_Field==j)/2;
     colourstemp=kIn.Intensity_MeanIntensity_DNA(kIn.Metadata_Field==j);
-
+    
     centersX =[centersX;centersXtemp];
     centersY =[centersY;centersYtemp];
     centersZ =[centersZ;centersZtemp];
-  
+    
     majors =[majors; majorstemp];
     minors= [minors; minorstemp];
     orientations=[orientations; orientationtemp];
@@ -67,82 +67,82 @@ rb=minors;
 ang=orientations;
 Nb=300;
 C=get(gca,'colororder');
+diff=length(unique(kIn.Metadata_Plane))-length(C(:,1));
 
+unique_z=unique(centersZ);
+
+if diff>0
+    
+    for i=1: diff
+        var=length(C(:,1))+i;
+        C(var,:)=C(1,:);
+    end
+end
 
 if length(ra)~=length(rb),
-  error('length(ra)~=length(rb)');
+    error('length(ra)~=length(rb)');
 end;
 if length(x0)~=length(y0),
-  error('length(x0)~=length(y0)');
+    error('length(x0)~=length(y0)');
 end;
 
 % how many inscribed elllipses are plotted
 
 if length(ra)~=length(x0)
-  maxk=length(ra)*length(x0);
+    maxk=length(ra)*length(x0);
 else
-  maxk=length(ra);
+    maxk=length(ra);
 end;
 
 % drawing loop
 
 for k=1:maxk
-  
-  if length(x0)==1
-    xpos=x0;
-    ypos=y0;
-    zpos=centersZ(k);
-    radm=ra(k);
-    radn=rb(k);
-    if length(ang)==1
-      an=ang;
+    
+    if length(x0)==1
+        xpos=x0;
+        ypos=y0;
+        zpos=centersZ(k);
+        radm=ra(k);
+        radn=rb(k);
+        if length(ang)==1
+            an=ang;
+        else
+            an=ang(k);
+        end;
+    elseif length(ra)==1
+        xpos=x0(k);
+        ypos=y0(k);
+        zpos=centersZ(k);
+        radm=ra;
+        radn=rb;
+        an=ang;
+    elseif length(x0)==length(ra)
+        xpos=x0(k);
+        ypos=y0(k);
+        zpos=centersZ(k);
+        radm=ra(k);
+        radn=rb(k);
+        an=ang(k);
     else
-      an=ang(k);
+        rada=ra(fix((k-1)/size(x0,1))+1);
+        radb=rb(fix((k-1)/size(x0,1))+1);
+        an=ang(fix((k-1)/size(x0,1))+1);
+        xpos=x0(rem(k-1,size(x0,1))+1);
+        ypos=y0(rem(k-1,size(y0,1))+1);
+        zpos=centersZ(k);
     end;
-  elseif length(ra)==1
-    xpos=x0(k);
-    ypos=y0(k);
-    zpos=centersZ(k);
-    radm=ra;
-    radn=rb;
-    an=ang;
-  elseif length(x0)==length(ra)
-    xpos=x0(k);
-    ypos=y0(k);
-    zpos=centersZ(k);
-    radm=ra(k);
-    radn=rb(k);
-    an=ang(k);
-  else
-    rada=ra(fix((k-1)/size(x0,1))+1);
-    radb=rb(fix((k-1)/size(x0,1))+1);
-    an=ang(fix((k-1)/size(x0,1))+1);
-    xpos=x0(rem(k-1,size(x0,1))+1);
-    ypos=y0(rem(k-1,size(y0,1))+1);
-    zpos=centersZ(k);
-  end;
-
-  co=cos(an);
-  si=sin(an);
-  the=linspace(0,2*pi,Nb(rem(k-1,size(Nb,1))+1,:)+1);
-  zpos=zeros(Nb+1,1);
-  zpos(:)=transpose(centersZ(k));
-  
- x=radm*cos(the)*co-si*radn*sin(the)+xpos;
- y=radm*cos(the)*si+co*radn*sin(the)+ypos;
-  h(k)=line(radm*cos(the)*co-si*radn*sin(the)+xpos,radm*cos(the)*si+co*radn*sin(the)+ypos,zpos);
-  set(h(k),'color',C(rem(k-1,size(C,1))+1,:));
+    
+    co=cos(an);
+    si=sin(an);
+    the=linspace(0,2*pi,Nb(rem(k-1,size(Nb,1))+1,:)+1);
+    zpos=zeros(Nb+1,1);
+    zpos(:)=transpose(centersZ(k));
+    
+    x=radm*cos(the)*co-si*radn*sin(the)+xpos;
+    y=radm*cos(the)*si+co*radn*sin(the)+ypos;
+    h(k)=line(radm*cos(the)*co-si*radn*sin(the)+xpos,radm*cos(the)*si+co*radn*sin(the)+ypos,zpos);
+    
+    currZpos=find(unique_z==zpos(1,1));
+        set(h(k),'color',C(currZpos,:));
 end;
-
 hold off
-%set working dir to xhtml exporter home
-%set options for figure2xhtml(), so that filename of output is
-%PLATEBARCODE_WELL, also set dir for export to user defined dir 
-%also user defined options for grid, axes name etc
- figure2xhtml()
- %reset working dir to the temp dir set by KNIME
-
-
-mOut=kIn;
-
-
